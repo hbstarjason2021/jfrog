fa561b1ac261be415cf277e23bc2a1834c5538d9
hbstarjason2021 <83358325+hbstarjason2021@users.noreply.github.com>
2021-11-13 13:17:50 +0800

    Update install-k3s.sh

--- install-k3s.sh
+++ install-k3s.sh
@@ -1,1 +1,7 @@
+curl -sfL https://get.k3s.io | sh -
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+k3s check-config
+
+### 国内安装k3s
+#curl -sfL http://rancher-mirror.cnrancher.com/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn sh -
